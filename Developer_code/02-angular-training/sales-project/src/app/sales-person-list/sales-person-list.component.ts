6423580b2b4e98de5b9d6d42002f509a36c8992d
Bhaskar Mudaliyar <mudaliyarBhaskar29@gmail.com>
2024-07-14 16:03:52 +0530

    angular training

---
+++ Developer_code/02-angular-training/sales-project/src/app/sales-person-list/sales-person-list.component.ts
@@ -0,0 +1,22 @@
+import { Component, OnInit } from '@angular/core';
+import { SalesPerson } from './sales-person';
+
+@Component({
+  selector: 'app-sales-person-list',
+  templateUrl: './sales-person-list.component.html',
+  styleUrls: ['./sales-person-list.component.css']
+})
+export class SalesPersonListComponent implements OnInit {
+
+  salesPersonList:SalesPerson[]=[
+    new SalesPerson("bhaskar","mudaliyar","mudaliyar@gmail.com",5000),
+    new SalesPerson("soni","mudaliyar","soni@gmail.com",6000),
+    new SalesPerson("kanishk","mudaliyar","kanishk@gmail.com",7000),
+    new SalesPerson("sammy","mudaliyar","sammy@gmail.com",4000),
+  ]
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
