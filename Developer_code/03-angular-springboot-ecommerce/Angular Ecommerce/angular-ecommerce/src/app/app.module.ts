0eaecb7b47d78d8e4d380e21e5dc517f81d2ff43
Bhaskar Mudaliyar <mudaliyarBhaskar29@gmail.com>
2024-07-23 20:54:49 +0530

    protected Routes

--- Developer_code/03-angular-springboot-ecommerce/Angular Ecommerce/angular-ecommerce/src/app/app.module.ts
+++ Developer_code/03-angular-springboot-ecommerce/Angular Ecommerce/angular-ecommerce/src/app/app.module.ts
@@ -1,11 +1,11 @@
-import { NgModule } from '@angular/core';
+import { Injector, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule} from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 import { ProductListComponent } from './components/product-list/product-list.component';
 import { ProductService } from './services/product.service';
-import { RouterModule, Routes } from '@angular/router';
+import { Router, RouterModule, Routes } from '@angular/router';
 import { ProductCategoryMenuComponent } from './components/product-category-menu/product-category-menu.component';
 import { SearchComponentComponent } from './components/search-component/search-component.component';
 import { ProductDetailsComponent } from './components/product-details/product-details.component';
@@ -17,17 +17,24 @@ import { ReactiveFormsModule } from '@angular/forms';
 import { LoginComponent } from './components/login/login.component';
 import { LoginStatusComponent } from './components/login-status/login-status.component';
 
-import {OKTA_CONFIG,OktaCallbackComponent, OktaAuthModule} from '@okta/okta-angular';
+import {OKTA_CONFIG,OktaCallbackComponent, OktaAuthModule, OktaAuthGuard} from '@okta/okta-angular';
 
 import {OktaAuth} from '@okta/okta-auth-js';
 import myAppConfig from './config/my-app-config';
+import { MembersComponent } from './components/members/members.component';
 
 const oktaConfig= myAppConfig.oidc;
 
 const oktaAuth= new OktaAuth(oktaConfig);
 
+function sendToLoginPage(oktaAuth:OktaAuth,injector:Injector){
+  const router= injector.get(Router);
+  router.navigate(['/login']);
+}
 const routes:Routes=[
 
+  
+  {path:"members",component:MembersComponent,canActivate:[OktaAuthGuard],data:{onAuthRequired:sendToLoginPage}},
   {path:"login/callback",component:OktaCallbackComponent},
   {path:"login",component:LoginComponent},
   {path:"checkout",component:CheckoutComponent},
@@ -53,7 +60,8 @@ const routes:Routes=[
     CartDetailsComponent,
     CheckoutComponent,
     LoginComponent,
-    LoginStatusComponent
+    LoginStatusComponent,
+    MembersComponent
   ],
   imports: [
     RouterModule.forRoot(routes),
